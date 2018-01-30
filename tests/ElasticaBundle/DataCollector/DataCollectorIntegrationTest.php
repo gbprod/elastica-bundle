1bcce883284741ed4db6d8c06ffc8bd67be4220c
Alexander Grimalovsky <alexander.grimalovsky@gmail.com>
2018-01-30 22:40:38 +0300

    Removed duplicated test as suggested by @gbprod

--- tests/ElasticaBundle/DataCollector/DataCollectorIntegrationTest.php
+++ tests/ElasticaBundle/DataCollector/DataCollectorIntegrationTest.php
@@ -7,14 +7,6 @@ use Tests\GBProd\KernelTestCase;
 
 class DataCollectorIntegrationTest extends KernelTestCase
 {
-    public function testDataCollectorIsRegisteredInContainer()
-    {
-        $container = self::$kernel->getContainer();
-        $this->assertTrue($container->has('elastica.data_collector'));
-        $service = $container->get('elastica.data_collector');
-        $this->assertInstanceOf(ElasticaDataCollector::class, $service);
-    }
-
     public function testDataCollectorIsRegisteredInProfiler()
     {
         $container = self::$kernel->getContainer();
