77882fecbd9b053dcf90df4400b0a30d381a40da
Alexander Grimalovsky <alexander.grimalovsky@gmail.com>
2018-01-31 17:45:17 +0300

    Custom directory removal code is replaced with use of Symfony Filesystem component as suggested by @gbprod

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,31 +1,6 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Cleanup previously created Symfony container and logs
-$rmDir = function ($path) {
-    if (!file_exists($path) || !is_dir($path)) {
-        return;
-    }
-    $dh = new RecursiveIteratorIterator(
-        new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS | FilesystemIterator::CURRENT_AS_FILEINFO),
-        RecursiveIteratorIterator::CHILD_FIRST
-    );
-    /** @var \SplFileInfo $file */
-    foreach ($dh as $file) {
-        if (!$file->isFile() && !$file->isLink()) {
-            continue;
-        }
-        unlink($file->getPathname());
-    }
-    $dh = new RecursiveIteratorIterator(
-        new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS | FilesystemIterator::CURRENT_AS_FILEINFO),
-        RecursiveIteratorIterator::CHILD_FIRST
-    );
-    foreach ($dh as $file) {
-        rmdir($file);
-    }
-    rmdir($path);
-};
-$rmDir(__DIR__ . '/Fixtures/cache');
-$rmDir(__DIR__ . '/Fixtures/logs');
-unset($rmDir);
+$fs = new \Symfony\Component\Filesystem\Filesystem();
+$fs->remove(__DIR__ . '/Fixtures/cache');
+$fs->remove(__DIR__ . '/Fixtures/logs');
